ffc1354899cb57ba53af82673ad7a67e41fc94ce
Ayman Khan <ayman.khan@and.digital>
2023-06-08 14:19:42 +0100

    test for errors

--- exchange.go
+++ exchange.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -21,23 +22,27 @@ type Query struct {
 	Amount 	float64 `json:"amount"`
 }
 
-func Exchange(amount float64, from, to, date string) (float64, error) {
-	
-	url := fmt.Sprintf("https://api.exchangerate.host/convert?from=%s&to=%s&amount=%f&date=%s", from, to, amount, date)
-	
-	response, err := http.Get(url)
-
+func HandleGetResponse(r *http.Response) (float64, error) {
+	responseData, err := io.ReadAll(r.Body)
 	if err != nil {
-		return 0, err
-	}
-
-	responseData, err := io.ReadAll(response.Body)
-	if err != nil {
-		return 0, err
+		return 0, errors.New("error reading the body")
 	}
 
 	var responseObject Response
 	json.Unmarshal(responseData, &responseObject)
 
 	return responseObject.Result, nil
+}
+
+func ExchangeGetRequest(amount float64, from, to, date string) (float64, error) {
+	
+	url := fmt.Sprintf("https://api.exchangerate.host/convert?from=%s&to=%s&amount=%f&date=%s", from, to, amount, date)
+	
+	response, err := http.Get(url)
+
+	if err != nil {
+		return 0, errors.New("error getting response")
+	}
+
+	return HandleGetResponse(response)
 }
--- exchange_test.go
+++ exchange_test.go
@@ -1,35 +1,49 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestExchange(t *testing.T) {
 
-	tests := map[string]struct {
-		amount float64
-		from string
-		to string
-		date string
-		want float64		
-	}{
-		"USD to GBP": {amount: 530, from: "USD", to: "GBP", date: "2023-06-08", want: 425.448008},
-		"GBP to USD": {amount: 342, from: "GBP", to: "USD", date: "2023-06-01", want: 428.845991},
-		"faulty params": {amount: 100, from: "invalid", to: "invalid", date: "invalid", want: 0},
-	}
+	t.Run("currency exchange", func(t *testing.T) {
+		tests := map[string]struct {
+			amount float64
+			from string
+			to string
+			date string
+			want float64		
+		}{
+			"USD to GBP": {amount: 530, from: "USD", to: "GBP", date: "2023-06-08", want: 425.448008},
+			"GBP to USD": {amount: 342, from: "GBP", to: "USD", date: "2023-06-01", want: 428.845991},
+			"invalid but well-formed returns 0": {amount: 100, from: "invalid", to: "invalid", date: "invalid", want: 0},
+		}
+	
+		for name, tc := range tests {
+			t.Run(name, func(t *testing.T) {
+				got, err := ExchangeGetRequest(tc.amount, tc.from, tc.to, tc.date)
+	
+				if err != nil {
+					t.Fatal("got error where there shouldn't be")
+				}
+	
+				if got != tc.want {
+					t.Errorf("got %f, want %f", got, tc.want)
+				}
+			})
+		}
+	})
 
-	for name, tc := range tests {
-		t.Run(name, func(t *testing.T) {
-			got, err := Exchange(tc.amount, tc.from, tc.to, tc.date)
+	t.Run("error with GET", func(t *testing.T) {
+		_, err := ExchangeGetRequest(400, "bl  ah", "blah", "blah")
+		want := "error getting response"
 
-			if err != nil {
-				t.Error("got error where there shouldn't be")
-			}
-
-			if got != tc.want {
-				t.Errorf("got %f, want %f", got, tc.want)
-			}
-		})
-	}
+		if err == nil {
+			t.Fatal("was expecting error and didn't get one")
+		}
+		
+		if err.Error() != want {
+			t.Errorf("got %v, want %v", err, want)
+		}
+	})
 }
-
-
-// Test errors that occur
